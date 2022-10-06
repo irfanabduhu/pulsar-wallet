f5b54f7fa907f0a8ba8e5d0bd4934b824ad5f740
Irfanul Hoque <Irfanul.Hoque@brainstation-23.com>
2022-10-06 13:06:04 +0600

    Integrated backend and frontend

---
+++ client/src/home/Register.jsx
@@ -0,0 +1,139 @@
+import { FormContainer } from "../styles";
+import { Button, Checkbox, FormControlLabel, TextField } from "@mui/material";
+import { useEffect, useState, useRef } from "react";
+import { isEmail, isAlpha, isStrongPassword } from "validator";
+import { useNavigate } from "react-router-dom";
+import ReCAPTCHA from "react-google-recaptcha";
+import axios from "axios";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [valid, setValid] = useState(false);
+  const [marked, setMarked] = useState(false);
+  const captchaRef = useRef(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setValid(
+      isEmail(email) &&
+        isValidUsername(username) &&
+        isValidPassword(password) &&
+        password === password2 &&
+        marked
+    );
+  }, [email, username, password, password2, marked]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const token = captchaRef.current.getValue();
+    captchaRef.current.reset();
+
+    try {
+      const PORT = process.env.PORT || 3001;
+      const { data } = await axios.post(
+        `http://localhost:${PORT}/api/register`,
+        {
+          email,
+          username,
+          password,
+          token,
+        }
+      );
+
+      if (data.success) {
+        navigate("/wallet", { state: { username: data?.payload?.username } });
+      }
+    } catch (err) {
+      console.error(err);
+      navigate("/", { state: { failed: "registration" } });
+    }
+  }
+  function isValidUsername(username) {
+    return (
+      username.length >= 8 &&
+      isAlpha(username) &&
+      /[a-z]/.test(username) &&
+      /[A-Z]/.test(username)
+    );
+  }
+  function isValidPassword(password) {
+    return isStrongPassword(password, {
+      minLength: 12,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    });
+    // Example: aB1@abracadabra
+  }
+
+  return (
+    <FormContainer>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          type="email"
+          label="Email"
+          size="small"
+          required
+          fullWidth
+          sx={{ margin: 0.5 }}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          helperText="Please enter a valid email."
+        />
+        <TextField
+          type="text"
+          label="Username"
+          size="small"
+          required
+          fullWidth
+          sx={{ margin: 0.5 }}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          helperText="Must include a lower and capital letter, at least 10 characters"
+        />
+        <TextField
+          type="password"
+          label="Password"
+          size="small"
+          required
+          fullWidth
+          sx={{ margin: 0.5 }}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          helperText="Must include at least a lowercase letter, an uppercase letter, a number, and a symbol. Must have at least 12 characters."
+        />
+        <TextField
+          type="password"
+          label="Confirm Password"
+          size="small"
+          required
+          fullWidth
+          sx={{ margin: 0.5 }}
+          value={password2}
+          onChange={(e) => setPassword2(e.target.value)}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox checked={marked} onChange={(e) => setMarked(!marked)} />
+          }
+          label="Do you agree with the terms and conditions?"
+          sx={{ margin: 0.5 }}
+        ></FormControlLabel>
+        <ReCAPTCHA
+          sitekey="6LdZwVciAAAAAB6QdY0Usriz7Zx0Nw1w3hYmYGQ6"
+          ref={captchaRef}
+        />
+        <Button type="submit" variant="contained" disabled={!valid}>
+          Register
+        </Button>
+      </form>
+    </FormContainer>
+  );
+}
+
+// Example: aB1@abracadabra
---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressSession = require("express-session");
+const dotenv = require("dotenv").config();
+const axios = require("axios");
+const path = require("path");
+const Recaptcha = require("express-recaptcha").RecaptchaV2;
+
+const auth = require("./auth/index");
+const api = require("./api");
+
+const app = express();
+const recaptcha = new Recaptcha(process.env.SITE_KEY, process.env.SECRET_KEY);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+    expressSession({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+app.use(auth.passport.initialize());
+app.use(auth.passport.session());
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+// async function authRecaptcha(req, res, next) {
+//     const { token } = req.body;
+//     const response = await axios({
+//         method: "GET",
+//         url: `https:///www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${token}`,
+//         headers: {
+//             "Access-Control-Allow-Origin": "*",
+//         },
+//     });
+// }
+
+app.post(
+    "/api/login",
+    recaptcha.middleware.verify,
+    (req, res, next) => {
+        if (!req.recaptcha.error) return next();
+        next(); // skip reCAPTCHA validation for now
+        // res.json({ success: false, payload: { error: "Recaptcha failed" } });
+    },
+    auth.passport.authenticate("local-signin"),
+    api.login
+);
+app.post("/api/register", api.register);
+app.get("/api/logout", api.logout);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
